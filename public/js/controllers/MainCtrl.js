6e1b9e943195ecfbc04d662eb25d5ad064bf6b8c
dcurrie <devin.currie@transcirrus.com>
2015-10-03 17:47:20 -0400

    begin homepage - added technologies

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -3,11 +3,17 @@
  Devin T. Currie
  */
 
-angular.module('MainCtrl', []).controller('MainController', function ($scope) {
-    $scope.page = "Home";
-    $scope.tagline = 'focused on developing sleek and intuitive user interfaces for web applications';
-    $scope.navlinks = [
-        {"value": "Examples", "id": "exams"},
-        {"value": "Technology", "id": "techs"},
-        {"value": "Specialities", "id": "specs"}];
-});
+angular.module('MainCtrl', [])
+    .controller('MainController', function ($scope) {
+        $scope.page = "Home";
+        $scope.tagline = 'focused on developing sleek and intuitive user interfaces for web applications';
+        $scope.links = [
+            {"value": "Examples", "id": "exams"},
+            {"value": "Technology", "id": "techs"},
+            {"value": "Specialities", "id": "specs"}];
+    })
+    .directive('mainNav', function () {
+        return {
+            templateUrl: 'navbar.html'
+        };
+    });
--- public/js/controllers/TestCtrl.js
+++ public/js/controllers/TestCtrl.js
@@ -3,7 +3,9 @@
  Devin T. Currie
  */
 
-angular.module('TestCtrl', []).controller('TestController', function ($scope) {
-    $scope.page = "Test";
-    $scope.tagline = 'this is a test page!';
-});
+angular.module('TestCtrl', [])
+    .controller('TestController', function ($scope) {
+        $scope.page = "Test";
+        $scope.tagline = 'this is a test page!';
+        $scope.links = [];
+    });
