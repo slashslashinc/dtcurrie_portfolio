b1be19463c49cad20a738a55ada832efb1f2f6f7
dcurrie <devin.currie@transcirrus.com>
2015-12-18 16:28:04 -0500

    updated app

--- public/js/app.js
+++ public/js/app.js
@@ -57,7 +57,8 @@ angular.module('MainCtrl', [])
         };
     });
 
-portfolio.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+portfolio.config(function ($stateProvider, $urlRouterProvider, $locationProvider, $urlMatcherFactory) {
+    $urlMatcherFactory.caseInsensitive(true);
     // For any unmatched url, redirect to /state1
     $urlRouterProvider.otherwise("/home");
     // Now set up the states
